4b52d4b400b6e61ef4e14b54c14d8936bcaebd94
Brit.Greg <brittanniagregorygmail.com>
2025-10-16 21:42:47 -0500

    Exercise 3 changes

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,7 +2,12 @@
 document.addEventListener('DOMContentLoaded',function(){
     let player = 'X';
     let board = Array(9).fill(null);
+    let gameover = false;
+    const winningCombos = [[0, 1, 2],[3, 4, 5],[6, 7, 8],[0, 3, 6],[1, 4, 7],
+    [2, 5, 8],[0, 4, 8],[2, 4, 6]];
+
     const squares = document.querySelectorAll('#board div');
+    const statusDiv = document.getElementById('status');
 
     function handleClickSquare(event){
         const square = event.currentTarget;
@@ -19,6 +24,19 @@ document.addEventListener('DOMContentLoaded',function(){
         square.addEventListener('mouseleave',handleMouseDormant);
         square.classList.remove('hover');
         
+        if (checkWinner(player)){
+            displayWinner(player);
+            gameover= true;
+            squares.forEach(s => s.removeEventListener('click',handleClickSquare));
+            return;
+        }
+
+        if(board.every(cell => cell !==null)){
+            statusDiv.textContent = "Its a Draw!";
+            gameover = true;
+            return;
+        }
+
         player = player === 'X'? 'O' : 'X';
     }
 
@@ -36,6 +54,18 @@ document.addEventListener('DOMContentLoaded',function(){
         const square = e.currentTarget;
         square.classList.remove('hover');
         square.classList.remove('X','O');
+    }
+
+    function checkWinner(player){
+        return winningCombos.some(combination =>{
+            const [a,b,c] = combination;
+            return board[a] === player && board[b] === player && board[c] === player;
+        });
+    }
+
+    function displayWinner(winner){
+        statusDiv.textContent = `Congratulations! ${winner} is the Winner!`;
+        statusDiv.classList.add('you-won');
     }
 
     squares.forEach(function(square,index){
