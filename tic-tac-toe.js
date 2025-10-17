080a01121b54a83ab4e9ca5849299ce249a4d6dd
Brit.Greg <brittanniagregorygmail.com>
2025-10-16 20:51:22 -0500

    Exercise 2 changes

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,22 @@
 "use strict";
 document.addEventListener('DOMContentLoaded',function(){
+    let player = 'X';
+    let board = Array(9).fill(null);
     const squares = document.querySelectorAll('#board div');
-    squares.forEach(function(square){
+    squares.forEach(function(square,index){
         square.classList.add('square');
+        square.setAttribute('data-index',index);
+        square.addEventListener('click',clickSquare);
     });
+    function clickSquare(event){
+        const square = event.currentTarget;
+        if(square.textContent === 'X' || square.textContent === 'O'){
+            return;
+        }
+        square.textContent = player;
+        square.classList.add(player);
+        const index = square.getAttribute('data-index');
+        board[index] = player;
+        player = player === 'X'? 'O' : 'X';
+    }
 });
